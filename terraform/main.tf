b1adbd7a6f7a4eedf4cd5037592f87287d15be41
CMPablos <cpablosr@gmail.com>
2025-03-14 15:10:20 -0700

    added terraform for bucket and bq

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+    credentials = file(var.credentials)
+    project     = var.project_name
+    region      = var.region
+  # Configuration options
+}
+
+
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = var.project_name
+  location      = var.location
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = false
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 1  // days
+    }
+  }
+
+  force_destroy = true
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "pageviews_dataset"
+  project     = var.project_name
+  location   = var.location
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,31 @@
+
+
+variable "credentials" {
+  description = "My Credentials"
+  default     = "./keys/my-credentials.json"
+}
+
+variable "project_name" {
+  description = "Project"
+  default     = "wikimedia-pageviews"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "wikimedia-pageviews-dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "wikimedia-pageviews-bucket"
+}
