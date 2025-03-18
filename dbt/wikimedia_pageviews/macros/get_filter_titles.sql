ea20d41e38f3de55a4f6a8d2f32b86a756fd10bb
CMPablos <cpablosr@gmail.com>
2025-03-17 20:32:06 -0700

    macro for page filtering fix

--- dbt/wikimedia_pageviews/macros/get_filter_titles.sql
+++ dbt/wikimedia_pageviews/macros/get_filter_titles.sql
@@ -1,16 +1,22 @@
 {% macro get_filter_titles() %}
-  {% set filter_titles = [
-    'Main_Page',
-    'Wikipedia:Portada',
-    'Wikipedia:Hauptseite',
-    'Wikipédia:Accueil_principal'
+  {% set main_pages = [
+    "Main_Page",
+    "Wikipedia:Portada",
+    "Wikipedia:Hauptseite",
+    "Wikipédia:Accueil_principal"
   ] %}
-  {% set search_titles = [
-    'Special:Search',
-    'Especial:Buscar',
-    'Spezial:Suche',
-    'Spécial:Recherche'
+  {% set search_pages = [
+    "Special:Search",
+    "Especial:Buscar",
+    "Spezial:Suche",
+    "Spécial:Recherche"
   ] %}
-  {{ return(filter_titles + search_titles) }}
+  {% set quoted_pages = [] %}
+
+  {% for page in main_pages + search_pages %}
+    {% do quoted_pages.append("'" ~ page ~ "'") %}
+  {% endfor %}
+
+  {{ return(quoted_pages | join(", ")) }}
 
 {% endmacro %}
