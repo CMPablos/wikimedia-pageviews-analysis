0edbe792d99e008df8a5c748914e1a2ce7e9f4a6
CMPablos <cpablosr@gmail.com>
2025-03-18 20:22:01 -0700

    separated empty pages from other filters

--- dbt/macros/get_filter_titles.sql
+++ dbt/macros/get_filter_titles.sql
@@ -3,21 +3,21 @@
     "Main_Page",
     "Wikipedia:Portada",
     "Wikipedia:Hauptseite",
-    "Wikipédia:Accueil_principal",
-    "-",
-    ""
+    "Wikipédia:Accueil_principal"
   ] %}
   {% set search_pages = [
     "Special:Search",
     "Especial:Buscar",
     "Spezial:Suche",
-    "Spécial:Recherche",
+    "Spécial:Recherche"
+  ] %}
+  {% set empty_pages = [
     "-",
     ""
   ] %}
   {% set quoted_pages = [] %}
 
-  {% for page in main_pages + search_pages %}
+  {% for page in main_pages + search_pages + empty_pages%}
     {% do quoted_pages.append("'" ~ page ~ "'") %}
   {% endfor %}
 
